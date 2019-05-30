d5cca200c15599a2a40d1304eae202ead1579f8d
Leonel Oliveira <leoneloliviera@Leonels-MacBook-Air.local>
2019-05-29 22:23:03 -0400

    UI updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,19 @@ var appFunctions = {
         },
         getMaps: function(){
             const address = document.getElementsByTagName("input")[0];
+            const map = document.getElementById("map");
             address.addEventListener("focusout", function (e) {
-                if (address.value.length > 0) {
-                        displayMap(address.value);
+                if (address.value.length > 5) {
+                    displayMap(address.value);
+                }else{
+                    map.classList.remove('open');
+                    map.innerHTML = "";
                 }
             });
             const displayMap = (local) => {
                 var componentMap = '<iframe width="90%" height="200" src="https://maps.google.com/maps?width=90%&amp;height=200px&amp;hl=en&amp;q='+local+'&amp;ie=UTF8&amp;t=&amp;z=12&amp;&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/create-google-map/">Add map to website</a></iframe>';
-                var map = document.getElementById("map");
                 map.innerHTML =  componentMap;
+                map.classList.add('open');
             }
         }
 	}			
